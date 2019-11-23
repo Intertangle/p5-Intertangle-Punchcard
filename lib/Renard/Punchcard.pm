a79e868294d9389befb0533642d269951eb75ee9
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-11-23 14:26:29 -0600

    Initial skeleton

---
+++ lib/Renard/Punchcard.pm
@@ -0,0 +1,12 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Punchcard;
+# ABSTRACT: A constraint modelling meta-system
+
+use Mu;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Punchcard/>
+
+=cut
